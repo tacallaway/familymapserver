d9524d34e5534b6e7960a8303c31773420b1f5e0
tacallaway <46659840+tacallaway@users.noreply.github.com>
2019-02-21 22:07:55 -0700

    Update schema.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,40 +1,46 @@
-CREATE TABLE "User" (
-    `UserName` TEXT NOT NULL UNIQUE,
-    `Password` TEXT NOT NULL,
-    PRIMARY KEY(`UserName`)
-)
-
-CREATE TABLE "Person" (
-    `PersonID` TEXT NOT NULL UNIQUE,
-    `Descendant` TEXT NOT NULL,
-    `FirstName` TEXT NOT NULL,
-    `LastName` TEXT NOT NULL,
-    `Gender` TEXT NOT NULL,
-    `Father` TEXT,
-    `Mother` TEXT,
-    `Spouse` TEXT,
-    FOREIGN KEY(`Descendant`) REFERENCES `User`(`UserName`),
-    PRIMARY KEY(`PersonID`)
-)
-
-CREATE TABLE "Event" (
-    `EventID` TEXT NOT NULL UNIQUE,
-    `Descendant` TEXT NOT NULL,
-    `PersonID` TEXT NOT NULL,
-    `Latitude` REAL NOT NULL,
-    `Longitude` REAL NOT NULL,
-    `Country` TEXT NOT NULL,
-    `City` TEXT NOT NULL,
-    `EventType` TEXT NOT NULL,
-    `Year` INTEGER NOT NULL,
-    FOREIGN KEY(`Descendant`) REFERENCES `User`(`UserName`),
-    PRIMARY KEY(`EventID`),
-    FOREIGN KEY(`PersonID`) REFERENCES `Person`(`PersonID`)
-)
-
-CREATE TABLE "AuthToken" (
-    `Token` TEXT NOT NULL UNIQUE,
-    `Username` TEXT NOT NULL,
-    PRIMARY KEY(`Token`),
-    FOREIGN KEY(`Username`) REFERENCES `User`(`UserName`)
-)
+CREATE TABLE `User` (
+	`UserName`	TEXT NOT NULL UNIQUE,
+	`Password`	TEXT NOT NULL,
+	`Email`	TEXT NOT NULL,
+	`FirstName`	TEXT NOT NULL,
+	`LastName`	TEXT NOT NULL,
+	`Gender`	TEXT CHECK(Gender IN ( "f" , "m" )),
+	`PersonID`	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY(`UserName`),
+	FOREIGN KEY(`PersonID`) REFERENCES `Person`(`PersonID`)
+);
+                           
+CREATE TABLE `Person` (
+	`PersonID`	TEXT NOT NULL UNIQUE,
+	`Descendant`	TEXT,
+	`FirstName`	TEXT NOT NULL,
+	`LastName`	TEXT NOT NULL,
+	`Gender`	TEXT NOT NULL CHECK(Gender IN ( "f" , "m" )),
+	`Father`	TEXT,
+	`Mother`	TEXT,
+	`Spouse`	TEXT,
+	FOREIGN KEY(`Descendant`) REFERENCES `User`(`UserName`),
+	PRIMARY KEY(`PersonID`)
+);
+                                    
+CREATE TABLE `Event` (
+	`EventID`	TEXT NOT NULL UNIQUE,
+	`Descendant`	TEXT NOT NULL,
+	`PersonID`	TEXT NOT NULL,
+	`Latitude`	REAL NOT NULL,
+	`Longitude`	REAL NOT NULL,
+	`Country`	TEXT NOT NULL,
+	`City`	TEXT NOT NULL,
+	`EventType`	TEXT NOT NULL,
+	`Year`	INTEGER NOT NULL,
+	PRIMARY KEY(`EventID`),
+	FOREIGN KEY(`Descendant`) REFERENCES `User`(`UserName`),
+	FOREIGN KEY(`PersonID`) REFERENCES `Person`(`PersonID`)
+);
+                                    
+CREATE TABLE `AuthToken` (
+	`Token`	TEXT NOT NULL UNIQUE,
+	`Username`	TEXT NOT NULL,
+	PRIMARY KEY(`Token`),
+	FOREIGN KEY(`Username`) REFERENCES `User`(`UserName`)
+);
